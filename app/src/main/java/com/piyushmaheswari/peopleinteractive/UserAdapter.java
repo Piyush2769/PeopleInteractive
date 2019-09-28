78523b098759e971e97e1d80559e18086d7e7e5d
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-28 13:33:33 +0530

    Initial +1

--- app/src/main/java/com/piyushmaheswari/peopleinteractive/UserAdapter.java
+++ app/src/main/java/com/piyushmaheswari/peopleinteractive/UserAdapter.java
@@ -1,26 +1,42 @@
 package com.piyushmaheswari.peopleinteractive;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    private Context context;
+    private List<User> userList;
+
+    public UserAdapter(Context context, List<User> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
 
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.list_item,null);
+        return new UserViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int i) {
 
+        User user=userList.get(i);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userList.size();
     }
 
     class UserViewHolder extends RecyclerView.ViewHolder{
